6a674600884615587a9db2c7946a4c4f191d38d6
Sourishharh <sourishharh@gmail.com>
2025-03-10 08:54:16 +0530

    changes

--- Script.js
+++ Script.js
@@ -1,13 +1,7 @@
-// function download{
-//     butt.download();
-// }
-
 const typed = new Typed('.multiple-Text',{
     strings:['Software Engineer', 'Data Analyst','Web Devoloper','Data Engineer','AI Researcher'],
     typeSpeed:73,
     backSpeed:73,
     backdelay:1000,
-    loop:true,
-    showCursor: true,  // Ensures the cursor is visible
-    cursorChar: '|',  
+    loop:true, 
 });
