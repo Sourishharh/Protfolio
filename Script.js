7c4bf2c05aa2d744436853da4be081e06cfd1e85
Sourishharh <sourishharh@gmail.com>
2025-03-10 11:58:04 +0530

    changes

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 const typed = new Typed('.multiple-Text',{
-    strings:['Software Engineer', 'Data Analyst','Web Devoloper','Data Engineer','AI Researcher'],
+    strings:['Software Engineer', 'Data Analyst','Web Developer','Data Engineer','AI Researcher'],
     typeSpeed:73,
     backSpeed:73,
     backdelay:1000,
